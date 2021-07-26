411bfd59c6955b989e3f79757f0e574ec116815f
Caitlin Swickard <caitlin6029@gmail.com>
2021-07-25 20:25:27 -0600

    filling in models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,7 +7,33 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      // References the Product model's id?? Did i do this right??
+      references: {
+        model: 'product',
+        key: 'id',
+        unique: false
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      // References the Tag model's id??
+      references: {
+        model: 'tag',
+        key: 'id',
+        unique: false
+      },
+    }
   },
+
+  // configurations
   {
     sequelize,
     timestamps: false,
